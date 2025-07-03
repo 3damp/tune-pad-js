194982e3329f129879c52cf9e833fcbe70650b0c
Armand <amp206@gmail.com>
2025-07-03 18:59:04 +0100

    refactor

---
+++ src/components/Pad.tsx
@@ -0,0 +1,41 @@
+import { NUM_NOTES, NUM_BEATS } from '../constants'
+import styles from './Pad.module.css'
+
+export default function Pad({
+  currentBeat,
+  enabledButtons,
+  toggleButton,
+}: {
+  currentBeat: number
+  enabledButtons: boolean[][]
+  toggleButton: (beatIdx: number, noteIdx: number) => void
+}) {
+  const classNames = (beatIdx: number, noteIdx: number) => {
+    const isActive = enabledButtons[beatIdx][noteIdx]
+    const isCurrentBeat = currentBeat === beatIdx
+    return `${styles.button} ${isActive ? styles.active : ''} ${isCurrentBeat ? styles.current : ''}`
+  }
+
+  return (
+    <div className={styles.container}>
+      {Array(NUM_NOTES)
+        .fill(0)
+        .map((_, noteIdx) => {
+          const reversedNoteIdx = NUM_NOTES - noteIdx - 1
+          return (
+            <div className={styles.buttonRow} key={reversedNoteIdx}>
+              {Array(NUM_BEATS)
+                .fill(0)
+                .map((_, beatIdx) => (
+                  <button
+                    key={`${beatIdx}-${reversedNoteIdx}`}
+                    className={classNames(beatIdx, reversedNoteIdx)}
+                    onClick={() => toggleButton(beatIdx, reversedNoteIdx)}
+                  ></button>
+                ))}
+            </div>
+          )
+        })}
+    </div>
+  )
+}
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const NUM_BEATS = 16
+export const NUM_NOTES = 16
+export const BPM = 120 // Beats per minute
