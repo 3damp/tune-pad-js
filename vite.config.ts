d7b08a93542c71de1f050e7358a6386a6ff801f2
Armand <amp206@gmail.com>
2025-07-07 17:56:46 +0100

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/random-name-generator/',
+  base: '/tune-pad-js/',
   resolve: {
     alias: {
       src: '/src',
